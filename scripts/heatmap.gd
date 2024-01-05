02cd14d03ba7c8fb0ae27b63ec93d334131cf185
okorn <>
2024-01-05 16:05:20 +1000

    Swap x and y in get_noise_2d() for Godot 4.x

--- scripts/heatmap.gd
+++ scripts/heatmap.gd
@@ -6,10 +6,9 @@ extends Sprite2D
 var xform_inv: Transform2D
 
 func _ready():
-    var xform = get_global_transform().translated(get_rect().position)
-    xform_inv = xform.affine_inverse()
+	var xform = get_global_transform().translated(get_rect().position)
+	xform_inv = xform.affine_inverse()
 
 func sample(global_pos: Vector2) -> float:
-    # NB: in godot 4.0, x and y should be swapped i.e. this will be get_noise_2d(pos.x, pos.y)
-    var local_pos = xform_inv * (global_pos)
-    return (noise.get_noise_2d(local_pos.y, local_pos.x) + 1.0) * 0.5
+	var local_pos = xform_inv * (global_pos)
+	return (noise.get_noise_2d(local_pos.x, local_pos.y) + 1.0) * 0.5
