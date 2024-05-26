2c88b859c617c646045f8c61e03405436b9765c2
Kylee <ido5490@gmail.com>
2024-05-26 17:18:49 +0300

    3dify

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,12 +1,12 @@
 # Coordinates the generation and drawing of road segments and buildings
 
-extends Node2D
+extends Node3D
 
 const CityGen = preload("res://scripts/city_gen.gd")
 
-@onready var city_gen: CityGen = $"../CityGen"
-@onready var population_heatmap = $"../PopulationHeatmap"
-@onready var options_menu = $"../UI/OptionsDialogue"
+@onready var city_gen: CityGen = $CityGen
+@onready var population_heatmap = $PopulationHeatmap
+@onready var options_menu = $UI/OptionsDialogue
 
 var generated_segments = []
 var generated_buildings = []
@@ -15,14 +15,22 @@ var rng = RandomNumberGenerator.new()
 func _ready():
 	populate_options_values()
 	run()
-
-func _draw():
+	
+func _process(delta: float) -> void:
+	draw()
+	
+func draw():
 	for segment in generated_segments:
 		var width = Options.HIGHWAY_SEGMENT_WIDTH if segment.metadata.highway else Options.NORMAL_SEGMENT_WIDTH
-		draw_line(segment.start, segment.end, Color8(161, 175, 165), width)
+		var s = DebugDraw3D.scoped_config().set_thickness(width/4)
+		DebugDraw3D.draw_line(Vector3(segment.start.x, 0, segment.start.y), Vector3(segment.end.x, 0, segment.end.y),Color8(161, 175, 165))
 	for building in generated_buildings:
-		var corners = building.generate_corners()
-		draw_colored_polygon(corners, Color8(12, 22, 31), [], null)
+		var corners2d = building.generate_corners()
+		var corners3d: PackedVector3Array = []
+		for corner in corners2d:
+			corners3d.append(Vector3(corner.x, 0, corner.y))
+		var s = DebugDraw3D.scoped_config().set_thickness(2)
+		DebugDraw3D.draw_lines(corners3d, Color8(12, 22, 31))
 
 func run():
 	for segment in generated_segments:
@@ -42,9 +50,7 @@ func run():
 
 	print("World seed: ", Options.WORLD_SEED)
 	refresh_world_seed_display()
-	
-	# trigger redraw
-	queue_redraw()
+
 
 
 ### GENERATE BUTTON & TOGGLES ###
@@ -113,41 +119,41 @@ func _on_world_seed_input_text_changed(new_value):
 	Options.WORLD_SEED = new_value
 
 func populate_options_values():
-	get_node("../UI/OptionsDialogue/SegmentsInput")\
+	options_menu.get_node("SegmentsInput")\
 		.set_text(str(Options.SEGMENT_COUNT_LIMIT))
-	get_node("../UI/OptionsDialogue/BuildingDensityInput")\
+	options_menu.get_node("BuildingDensityInput")\
 		.set_text(str(Options.BUILDING_COUNT_PER_SEGMENT))
-	get_node("../UI/OptionsDialogue/BuildingFrequencyInput")\
+	options_menu.get_node("BuildingFrequencyInput")\
 		.set_text(str(Options.BUILDING_SEGMENT_PERIOD))
-	get_node("../UI/OptionsDialogue/SegmentLengthInput")\
+	options_menu.get_node("SegmentLengthInput")\
 		.set_text(str(Options.DEFAULT_SEGMENT_LENGTH))
-	get_node("../UI/OptionsDialogue/HighwaySegmentLengthInput")\
+	options_menu.get_node("HighwaySegmentLengthInput")\
 		.set_text(str(Options.HIGHWAY_SEGMENT_LENGTH))
-	get_node("../UI/OptionsDialogue/DefaultBranchProbabilityInput")\
+	options_menu.get_node("DefaultBranchProbabilityInput")\
 		.set_text(str(Options.DEFAULT_BRANCH_PROBABILITY))
-	get_node("../UI/OptionsDialogue/HighwayBranchProbabilityInput")\
+	options_menu.get_node("HighwayBranchProbabilityInput")\
 		.set_text(str(Options.HIGHWAY_BRANCH_PROBABILITY))
-	get_node("../UI/OptionsDialogue/BranchAngleDeviationInput")\
+	options_menu.get_node("BranchAngleDeviationInput")\
 		.set_text(str(Options.BRANCH_ANGLE_DEVIATION))
-	get_node("../UI/OptionsDialogue/StraightAngleDeviationInput")\
+	options_menu.get_node("StraightAngleDeviationInput")\
 		.set_text(str(Options.STRAIGHT_ANGLE_DEVIATION))
-	get_node("../UI/OptionsDialogue/MinIntersectionDeviationInput")\
+	options_menu.get_node("MinIntersectionDeviationInput")\
 		.set_text(str(Options.MINIMUM_INTERSECTION_DEVIATION))
-	get_node("../UI/OptionsDialogue/BranchPopulationThresholdInput")\
+	options_menu.get_node("BranchPopulationThresholdInput")\
 		.set_text(str(Options.NORMAL_BRANCH_POPULATION_THRESHOLD))
-	get_node("../UI/OptionsDialogue/HighwayPopulationThresholdInput")\
+	options_menu.get_node("HighwayPopulationThresholdInput")\
 		.set_text(str(Options.HIGHWAY_BRANCH_POPULATION_THRESHOLD))
-	get_node("../UI/OptionsDialogue/MaxSnapDistanceInput")\
+	options_menu.get_node("MaxSnapDistanceInput")\
 		.set_text(str(Options.MAX_SNAP_DISTANCE))
-	get_node("../UI/OptionsDialogue/MaxBuildingDistanceInput")\
+	options_menu.get_node("MaxBuildingDistanceInput")\
 		.set_text(str(Options.MAX_BUILDING_DISTANCE_FROM_SEGMENT))
-	get_node("../UI/OptionsDialogue/NormalSegmentWidthInput")\
+	options_menu.get_node("NormalSegmentWidthInput")\
 		.set_text(str(Options.NORMAL_SEGMENT_WIDTH))
-	get_node("../UI/OptionsDialogue/HighwaySegmentWidthInput")\
+	options_menu.get_node("HighwaySegmentWidthInput")\
 		.set_text(str(Options.HIGHWAY_SEGMENT_WIDTH))
 
 func refresh_world_seed_display():
-	get_node("../UI/SeedInputHBoxContainer/WorldSeedInput")\
+	get_node("UI/SeedInputHBoxContainer/WorldSeedInput")\
 		.set_text(str(Options.WORLD_SEED))
 
 
